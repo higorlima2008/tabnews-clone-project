f3cf37ff5f7b5f86743d6caf7e941aecfccc0e1f
Higor Lima <113942613+higorlima2008@users.noreply.github.com>
2023-04-29 14:33:30 +0000

    Adiciona a pasta pages para a Página home

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+function Count (){
+    const [contador, setContador] = useState(0);
+    
+    const Click = () => {
+        setContador(contador + 1);
+    }
+
+    return(
+        <div>
+            <div>{contador}</div>
+            <button onClick={Click}>Clicar</button>
+        </div>
+    )
+}
+
+function Home(){
+    return <Count/>
+}
+
+export default Home;
